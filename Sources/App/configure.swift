0b1f5db058df738539660023c39c45e913e21383
Omar Ashraf <oashrafouad@gmail.com>
2025-02-15 03:51:37 +0200

    hide database details

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,25 +1,25 @@
 import Vapor
 import Fluent
 import FluentPostgresDriver
+import NIOSSL
 
 public func configure(_ app: Application) async throws {
-    // Set log level to debug
-    app.logger.logLevel = .debug
-
-    // Database configuration
+    app.logger.logLevel = .debug  // Set to debug temporarily
+    
     let postgresConfig = SQLPostgresConfiguration(
-        hostname: "***REMOVED***",
-        port: ***REMOVED***,
-        username: "***REMOVED***",
-        password: "***REMOVED***",
-        database: "postgres",
+        hostname: Environment.get("DB_HOST") ?? "localhost",
+        port: Environment.get("DB_PORT").flatMap(Int.init) ?? 5432,
+        username: Environment.get("DB_USERNAME") ?? "postgres",
+        password: Environment.get("DB_PASSWORD") ?? "postgres",
+        database: Environment.get("DB_NAME") ?? "postgres",
         tls: .disable
     )
+    
     app.databases.use(.postgres(configuration: postgresConfig), as: .psql)
-
+    
     // Register migrations
     app.migrations.add(CreateJoke())
-
+    
     // Register routes
     try routes(app)
 }
